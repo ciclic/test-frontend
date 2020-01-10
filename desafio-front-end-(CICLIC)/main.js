2745c36feeeeb321881c1fa06632c39af7dc8ab6
André Alves <andre.jobs23@gmail.com>
2020-01-10 14:31:04 -0300

    Atualização 2#
    API está funcional, basta apenas renderizar a mensagem na tela e estilizar.

--- desafio-front-end-(CICLIC)/main.js
+++ desafio-front-end-(CICLIC)/main.js
@@ -1,3 +1,35 @@
-document.getElementById('simulador').onclick = function teste(){
-    alert('alo alo')
+var app = document.getElementById('app');
+
+document.getElementById('simulador').onclick= function() {
+    var name = document.getElementById('nome').value;
+    var mensal = document.getElementById('mes').value;
+    var time = document.getElementById('contri').value;
+
+    axios.post('https://api.mathjs.org/v4/', {
+        "expr":
+            mensal * ((( 1 + 0.00517) ** time -1) / 0.00517),
+        "precision": 5
+    })
+        .then(response =>{
+            console.log(`Olá ${name}, juntando R$ ${mensal} todo mês, você terá R$ ${response.data.result} em ${time} meses`);
+        })
+        .catch(error =>{
+            console.log('error')
+        })
+
+        /*  testes
+        app.style.display='none';
 }
+
+function mensagem(){
+    var texto = document.createElement('p')
+    var conteudo = document.createTextNode('Funcionando');
+    texto.appendChild(conteudo);
+
+}  */
+        
+
+
+
+
+    
